e34ad036ce92cbd5fd0bc5e1cfbbea132178003a
Aman <99051262+Aman96-star@users.noreply.github.com>
2024-05-15 23:56:44 +0530

    responsive

---
+++ script.js
@@ -0,0 +1,58 @@
+const name = "nmeasfdsfszsasxzef"
+const age ='23'
+
+// console.log(name+age)
+
+const com =name+age
+// string interpretation method `` symbol
+console.log(`my name is ${name} and age is a ${age}`)
+
+
+// another method to make string 
+const name_1 =new String("dcjsifhishfoso")
+console.log(name_1)
+
+console.log(name_1.length)
+console.log(name_1.toUpperCase())
+//no chane in origional string 
+console.log(name_1.indexOf('j'))
+
+const currentDate =new Date();
+let year =currentDate.getFullYear();
+let month =currentDate.getMonth();
+let day =currentDate.getDate();
+let hour =currentDate.getHours();
+let minute =currentDate.getMinutes();
+let second =currentDate.getSeconds();
+
+console.log(currentDate )
+// console.log(second)
+
+
+const arr =[1,3234,2,5]
+console.log(arr.join(' '))
+
+// const data = ['Jan', 'March', 'April', 'June'];
+// // ,[12,343,234]
+// const data_2 = ['Jn', 'arch', 'tu', 'une'];
+// // ,[1,43,34]
+
+
+const d_1 =[1,2,3,4,5];
+const d_2 =[23,24,55,57];
+const dd = d_1.concat(d_2);
+console.log(dd)
+ 
+// another method to  concatinate 
+const demon = [...d_1,...d_2];
+console.log(demon)
+
+
+const jsuser ={
+    name:"anaa",
+    age :24,
+    state :"uttrakhand"
+}
+// two method to access a object 
+// 1. console.log(jsuser["name"])
+// 2. console.log(jsuser.name)
